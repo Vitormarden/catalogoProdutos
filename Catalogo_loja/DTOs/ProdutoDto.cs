a9ff1497ac1a767a16ba9a5262aeb40b2d9cd35b
VitorMarden <vitomarden1234@gmail.com>
2026-03-20 19:36:27 -0300

    feat: adicionanando Dtos

---
+++ Catalogo_loja/DTOs/ProdutoDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalogo_loja.DTOs;
+
+public class ProdutoDto
+{
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(100)]
+    public string Nome { get; set; } = string.Empty;
+
+    public string? Descricao { get; set; }
+
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public decimal Preco { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int Estoque { get; set; }
+
+    [Required]
+    public string Categoria { get; set; } = string.Empty;
+
+    public string? ImagemUrl { get; set; }
+
+    public bool Ativo { get; set; } = true;
+}
