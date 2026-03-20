88ead96ae24e70b770fc594ca1718f1074fb2e2b
VitorMarden <vitomarden1234@gmail.com>
2026-03-19 21:43:55 -0300

    feat: adicionando add app dbcontext e json do dataset

---
+++ Catalogo_loja/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Catalogo_loja.Models;
+
+namespace Catalogo_loja.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Produto> Produtos => Set<Produto>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configuração de precisão para valores monetários
+        modelBuilder.Entity<Produto>()
+            .Property(p => p.Preco)
+            .HasPrecision(18, 2);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
